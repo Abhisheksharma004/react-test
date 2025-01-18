5d88162603d3c89a22c3f4dfaf972b448d48c7d9
Abhisheksharma004 <abhishekkumarranjan965@gmail.com>
2025-01-19 00:46:09 +0530

    first commit

---
+++ react-blog/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+
+  server: {
+    host: '0.0.0.0', // Accept connections from any IP
+    port: 5173,      // Ensure the port matches your dev server
+  },
+})
+
+
