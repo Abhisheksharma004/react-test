6d26f183074c2756e175292cb85887e74db0f6e3
Abhisheksharma004 <abhishekkumarranjan965@gmail.com>
2025-02-09 22:30:45 +0530

    appwrite blog

---
+++ blogAppWrite/src/appwrite/config.js
@@ -0,0 +1,116 @@
+import conf from "../conf.js";
+import { Clinent, ID, Databases, Storage, Query } from "appwrite";
+
+export class Service {
+    client = new Clinent();
+    databases;
+    bucket;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.databases = new Databases(this.client);
+        this.bucket = new Storage(this.client);
+    }
+    async createPost({title, slug, content, featuredImage, status, userId}){
+        try {
+            return await this.databases.createDocument(
+                conf.appwriteProjectId,
+                conf.appwriteCollectionID,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId,
+                }
+            )
+        } catch (error) {
+            throw error;
+        }
+    }
+    async updatePost(slug, {title, content, featuredImage, status}){
+        try {
+            return await this.databases.updateDocument(
+                conf.appwriteDatabseId,
+                conf.appwriteCollectionID,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                }
+            )
+        } catch (error) {
+            throw error;
+        }
+    }
+    async deletePost(slug){
+        try {
+            await this.databases.deleteDocument(
+                conf.appwriteDatabseId,
+                conf.appwriteCollectionID,
+                slug,
+            )
+            return true
+        } catch (error) {
+            throw error;
+            return false
+        }
+    }
+    async getPost(slug){
+        try {
+            return await this.databases.getDocument(
+                conf.appwriteDatabseId,
+                conf.appwriteCollectionID,
+                slug,
+            )
+        } catch (error) {
+            throw error;
+            return false
+        }
+    }
+    async getPosts(queries = [Query.equal("status", "active")]){
+        try {
+            return await this.databases.listDocuments(
+                conf.appwriteDatabseId,
+                conf.appwriteCollectionID,
+                queries,
+            )
+        } catch (error) {
+            throw error;
+            return false
+        }
+    }
+    async uploadFile(file){
+        try {
+            return await this.bucket.createFile(
+                conf.appwriteBuketId,
+                ID.unique(),
+                file,
+            )
+        } catch (error) {
+            throw error;
+            return false
+        }
+    }
+    async deleteFile(fileId){
+        try {
+            await this.bucket.deleteFile(
+                conf.appwriteBuketId,
+                fileId
+            )
+            return true
+        } catch (error) {
+            throw error;
+            return false
+        }
+    }
+}
+
+
+const service = new Service()
+export default service
